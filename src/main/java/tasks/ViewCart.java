15623ba5c16bf86b686284d422a0dbebbc36960b
Christian <christianespinoza935@gmail.com>
2026-04-18 18:50:53 -0500

    Se modifca viewCart

--- src/main/java/tasks/ViewCart.java
+++ src/main/java/tasks/ViewCart.java
@@ -5,6 +5,9 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class ViewCart implements Task {
 
@@ -15,6 +18,10 @@ public class ViewCart implements Task {
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
                 Click.on(CheckoutPage.BTN_VIEW_CART),
+
+                WaitUntil.the(CheckoutPage.BTN_CHECKOUT, isVisible())
+                                .forNoMoreThan(15).seconds(),
+
                 Click.on(CheckoutPage.BTN_CHECKOUT)
         );
 
